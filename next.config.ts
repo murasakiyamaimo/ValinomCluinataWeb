0f3359b3df7293a9d0c22ca5331063f71004f1bd
BuildTools <unconfigured@null.spigotmc.org>
2025-06-27 16:18:45 +0900

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isExport = process.env.NEXT_EXPORT === 'true';
+
 const nextConfig: NextConfig = {
   /* config options here */
     output:"export",
+    basePath: isExport ? '/murasakiyamaimo.github.io' : '',
+    assetPrefix: isExport ? '/murasakiyamaimo.github.io/' : '',
+    trailingSlash: true,
 };
 
 export default nextConfig;
