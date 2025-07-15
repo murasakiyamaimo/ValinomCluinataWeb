065db1f1418c3660a24956116b9493418f439f2d
BuildTools <unconfigured@null.spigotmc.org>
2025-07-16 01:05:34 +0900

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,13 @@ export const metadata: Metadata = {
   description: "ValinomCluinata editor for browser",
   icons: {
       icon: [
-          { url: '/favicon.ico'},
-          { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-          { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+          { url: '/favicons/favicon.ico'},
+          { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+          { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       ],
-      apple: 'apple-touch-icon.png',
+      apple: '/favicons/apple-touch-icon.png',
   },
-  manifest: '/site.webmanifest',
+  manifest: '/favicons/site.webmanifest',
 };
 
 export default function RootLayout({
